9e0468d3930326e2ccc5852122cedc549014c486
Italo SF <italosilvafernandesprog@gmail.com>
2024-05-04 10:04:21 -0300

    Adicionando a pagina de sobre nós

--- paginas/views.py
+++ paginas/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from categorias.models import EstadoDeConservacaoModels, CategoriaModels
-from .models import HomeModels, QualidadesHomeModels
+from .models import HomeModels, QualidadesHomeModels, SobreNosModels, FuncionarioModels
 from django.views.generic import View
 from carro.models import CarroModels
 
@@ -26,3 +26,23 @@ class HomeIndex(View):
         }
 
         return render(self.request, 'home/index.html', context)
+
+
+class SobreNosIndex(View):
+
+    def get(self, *args, **kwargs):
+        personalizacoes = SobreNosModels.objects.filter(
+            publicado_sobre_nos=True).order_by('-id').first()
+        funcionarios = FuncionarioModels.objects.filter(
+            publicado=True).order_by('-id')
+        estados_de_conservacao = EstadoDeConservacaoModels.objects.all()
+        categorias = CategoriaModels.objects.all()
+
+        context = {
+            'personalizacoes': personalizacoes,
+            'funcionarios': funcionarios,
+            'estados_de_conservacao': estados_de_conservacao,
+            'categorias': categorias,
+        }
+
+        return render(self.request, 'sobre_nos/index.html', context)
